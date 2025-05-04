4f3eaa0d20001f964ef6c1f78c3e2f812f5ebc0b
JumpingHippy <86410250+JumpingHippy@users.noreply.github.com>
2025-05-04 09:08:55 -0400

    Update spider6.sh

--- spider6.sh
+++ spider6.sh
@@ -61,46 +61,9 @@ crawl_url() {
     links+=("$onion_url")
   done < <(grep -oE '<a href="(http|https)://[a-z2-7]{56}\.onion[^"]*"' "$OUTPUT_DIR/$filename" | sed -E 's/<a href="//;s/"$//')
   
-
-  ### Extract potential hidden onions -- THIS IS NOW OFF AS IT IS A PAIN IN THE BUTT. PROBABLy better to do
-  ### as a part of post-processing
-  ### refer to spider5 for details. TODO this line cannot stay for any serious commit and needs to be RESOLVED. 
-  
-  # Sort the links placing the origin site last
-  #
- 
    # Extract domain from current_url
    current_domain=$(echo "$url" | awk -F[/:] '{print $4}')
    echo "Current url: $url"
-
-   # Sort URLs: First those NOT matching the current domain, then the ones that match
-   #$sorted_links=($(printf "%s\n" "${links[@]}" | awk -v domain="$current_domain" '{
-   #    if ($0 ~ domain) {
-   #        print $0 > "current_domain_links.txt"
-   #    } else {
-   #        print
-   #    }
-   #}
-   #END {
-   #    while ((getline line < "current_domain_links.txt") > 0) {
-   #        print line
-   #    }
-   # close("current_domain_links.txt")
-#}'))
-
-# Print links
-#for link in "${links[@]}"; do
-#	echo "Link: $link" >> links_$sites_crawled.txt
-#done
-#for link in "${sorted_links[@]}"; do
-#	echo "Sorter link: $link" >> sort_links_$sites_crawled.txt
-#done
-
-#printf "%s\n" "Links: ${links[@]}"
-#printf "%s\n" "Sorted links: ${sorted_links[@]}"
-
-# Cleanup temporary file
-#rm -f current_domain_links.txt
 
   # Process extracted links
   if [ ${#links[@]} -gt 0 ]; then
